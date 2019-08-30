e7de0d51dbb5d101f0fab856a7f2659c40d1d6b8
Full Stack Web Developer <josef779@outlook.com>
2019-08-30 08:25:09 -0700

    Update viz.py

--- superset/viz.py
+++ superset/viz.py
@@ -333,29 +333,29 @@ class BaseViz(object):
             self.get_payload(),
             default=utils.json_int_dttm_ser, ignore_nan=True)
 
-    def cache_key(self, query_obj, **extra):
-        """
-        The cache key is made out of the key/values in `query_obj`, plus any
-        other key/values in `extra`.
-
-        We remove datetime bounds that are hard values, and replace them with
-        the use-provided inputs to bounds, which may be time-relative (as in
-        "5 days ago" or "now").
-
-        The `extra` arguments are currently used by time shift queries, since
-        different time shifts wil differ only in the `from_dttm` and `to_dttm`
-        values which are stripped.
-        """
-        cache_dict = copy.copy(query_obj)
-        cache_dict.update(extra)
-
-        for k in ['from_dttm', 'to_dttm']:
-            del cache_dict[k]
-
-        cache_dict['time_range'] = self.form_data.get('time_range')
-        cache_dict['datasource'] = self.datasource.uid
-        json_data = self.json_dumps(cache_dict, sort_keys=True)
-        return hashlib.md5(json_data.encode('utf-8')).hexdigest()
+    # def cache_key(self, query_obj, **extra):
+    #     """
+    #     The cache key is made out of the key/values in `query_obj`, plus any
+    #     other key/values in `extra`.
+    #
+    #     We remove datetime bounds that are hard values, and replace them with
+    #     the use-provided inputs to bounds, which may be time-relative (as in
+    #     "5 days ago" or "now").
+    #
+    #     The `extra` arguments are currently used by time shift queries, since
+    #     different time shifts wil differ only in the `from_dttm` and `to_dttm`
+    #     values which are stripped.
+    #     """
+    #     cache_dict = copy.copy(query_obj)
+    #     cache_dict.update(extra)
+    #
+    #     for k in ['from_dttm', 'to_dttm']:
+    #         del cache_dict[k]
+    #
+    #     cache_dict['time_range'] = self.form_data.get('time_range')
+    #     cache_dict['datasource'] = self.datasource.uid
+    #     json_data = self.json_dumps(cache_dict, sort_keys=True)
+    #     return hashlib.md5(json_data.encode('utf-8')).hexdigest()
 
     def get_payload(self, query_obj=None):
         """Returns a payload of metadata and data"""
